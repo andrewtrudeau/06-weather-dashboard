9bb86223f00e701d7f136d25aec795168e20a615
TRUDE135 <72237941+TRUDE135@users.noreply.github.com>
2021-09-30 23:29:45 -0500

    changes in readme

---
+++ Assets/js/script.js
@@ -0,0 +1,231 @@
+const apiKey = "77a5f5e9021668e3908aa9d8669df015"
+
+// List of city names, populates if there is something in localStorage //
+
+var cityNames = [];
+
+if (localStorage.getItem("cityNames")) {
+
+    cityNames = localStorage.getItem("cityNames").split(',');
+
+    for (let i = 0; i < cityNames.length; i++)
+        addCityButton(cityNames[i]);
+
+}
+
+//////////////////////
+// Helper Functions //
+//////////////////////
+
+// Changes: john smith --> John Smith //
+
+let toTitleCase = (str) => {
+    return str.replace(
+        /\w\S*/g,
+        function (txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
+
+// The API gives us a code and a url to get the image given the code //
+
+let getIconURL = code => `http://openweathermap.org/img/wn/${code}@2x.png`;
+
+// The api gives us an ugly date, but we can fix that //
+
+let prettyDate = ugly => {
+    let year = ugly.substring(0, 4);
+    let month = ugly.substring(5, 7);
+    let day = ugly.substring(8, 11);
+    if (day[0] === '0') day = day[1];
+
+    return month + "/" + day + "/" + year;
+
+}
+
+// Api docs state that units are in kelvin, we must convert to fahrenheit //
+
+let fahrenheit = kelvin => (kelvin - 273.15) * 9 / 5 + 32;
+
+// Api docs state that units are in meters per second, we must convert to MPH //
+
+let milesPerHour = metersPerSecond => metersPerSecond * 2.237;
+
+// This updates the CSS to set the background color of UVI based on UVI value //
+
+let uviClass = uvi => {
+    if (uvi < 3)
+        return "uvi-low"
+    if (uvi < 8)
+        return "uvi-moderate"
+    if (uvi < 11)
+        return "uvi-high"
+    else
+        return "uvi-extreme"
+};
+
+// Show or hide data based on whether a location that is selected is valid //
+
+function showData() {
+    $(".card").show();
+    $(".five-day").show();
+    $(".sectionTitle").show();
+}
+
+function hideData() {
+    $(".card").hide();
+    $(".five-day").hide();
+    $(".sectionTitle").hide();
+}
+
+// Get UVI data from separate api call, why is it separate? couldn't tell yah //
+
+let queryUVI = (lat, lon) => "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat +
+    "&lon=" + lon +
+    "&exclude={part}&appid=" + apiKey;
+
+///////////////////
+// Page Builders //
+///////////////////
+
+// Update local storage and location list //
+
+function addCity(cityName) {
+
+    // Lets standardize the city name to make it more 
+    // recognizable so we don't get duplicates based on
+    // one different character.
+    let prettyCityName = toTitleCase(cityName);
+
+    // Add button to html
+    if (!cityNames.includes(prettyCityName)) {
+        addCityButton(prettyCityName);
+        cityNames.push(prettyCityName);
+    }
+
+    // Update local storage
+    localStorage.setItem("cityNames", cityNames);
+}
+
+// This code adds a new location and makes a listener for it //
+
+function addCityButton(cityName) {
+    let id = cityName.replace(/\s+/g, '-').toLowerCase();
+
+    $('#drop-down').append(`<a href="#" id="${id}">${cityName}</a>`);
+
+    $('#' + id).click((event) => {
+        event.preventDefault();
+        getWeather(event.currentTarget.textContent);
+
+    });
+
+}
+
+// Add HTML of card to dom //
+
+function getCardHTML(date, iconCode, temp, windSpeed, humidity) {
+
+    return `
+        <div class="card">
+            ${getCardBodyHTML(date, iconCode, temp, windSpeed, humidity)}
+        </div>
+    `;
+}
+
+function getCardBodyHTML(date, iconCode, temp, windSpeed, humidity) {
+    let imgSrc = getIconURL(iconCode);
+
+    return `
+        <img src="${imgSrc}" alt="Weather Image">
+        <p>${prettyDate(date)}</p>
+        <p>${temp.toFixed(2)} °F</p>
+        <p>Wind: ${windSpeed.toFixed(2)} MPH</p>
+        <p>Humidity: ${humidity}%</p>
+    `;
+}
+
+function buildForecast(data, uvi) {
+
+    // Clear cards
+    $('#largeCard').empty();
+    $('#fiveDay').empty();
+
+    // Large Card
+    let date = data.list[0].dt_txt;
+    let iconCode = data.list[0].weather[0].icon;
+    let temp = fahrenheit(data.list[0].main.temp);
+    let windSpeed = milesPerHour(data.list[0].wind.speed);
+    let humidity = data.list[0].main.humidity;
+
+    $('#largeCard').append(getCardBodyHTML(date, iconCode, temp, windSpeed, humidity));
+
+    // Large card has UVI, small ones do not
+    $('#largeCard').append(`<p>UV Index: <span class="uvi ${uviClass(uvi)}">${uvi}</span></p>`);
+
+    // The rest of cards
+    for (let i = 7; i < 40; i += 8) {
+        date = data.list[i].dt_txt;
+        iconCode = data.list[i].weather[0].icon;
+        temp = fahrenheit(data.list[i].main.temp);
+        windSpeed = milesPerHour(data.list[i].wind.speed);
+        humidity = data.list[i].main.humidity;
+
+        $('#fiveDay').append(getCardHTML(date, iconCode, temp, windSpeed, humidity));
+
+    }
+
+    showData();
+
+}
+
+// Begs the server for the information to build our website //
+
+function getWeather(cityName) {
+
+    var queryURLForecast = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName +
+        "&appid=" + apiKey;
+
+
+    fetch(queryURLForecast).then(response => response.json())
+        .then(data => {
+            if (data.cod != 404) {
+
+                fetch(queryUVI(data.city.coord.lat, data.city.coord.lon))
+                    .then(res => res.json())
+                    .then(dataUV => {
+
+                        buildForecast(data, dataUV.current.uvi);
+
+                    });
+
+                // Update HTML h1 to location name
+                $("#location").text(toTitleCase(cityName));
+
+                // Add city data to local storage 
+                addCity(cityName);
+
+            } else
+                hideData();
+
+        });
+
+
+
+}
+
+// Click the submit button to access that location's data //
+
+$("#submit").click((event) => {
+    event.preventDefault();
+
+    getWeather($('#cityInput').val());
+
+    $('#cityInput').val(""); // Clear text box value
+});
+
+// Hide data on page load //
+
+hideData();
+
--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# 06-weather-dashboard
+# Homework 6: Weather Dashboard
+
+## Objective 
+
+This webpage allows the user to search a city and receive weather data about that city.
+
+## Changes
+
+I created the HTML, JS, and CSS from scratch. I used JQuery and a CSS library for special icons (linked in HTML @ ln. 9). The idea is: You have a relatively bare-bones HTML doc that has a couple sets of cards and a nav. The nav allows you to search and add new cities, and the <em>cites drop-down menu</em>  uses ```localStorage``` to save the viewed locations. The rest of the code uses ```fetch``` to collect the API data, and JQuery is used to select HTML elements to fill with the gained data from ```fetch```.
+
+## Code
+
+This code snippet highlights how I added code to the HTML sections. ```getCardBodyHTML``` & ```getCardHTML``` functionally retrieve the html necessary to make cards appear on the screen (the former function being for a large card and the later is used for the 5-day-forecast cards). 
+```JavaScript
+function buildForecast(data, uvi) {
+
+    // Clear cards
+    $('#largeCard').empty();
+    $('#fiveDay').empty();
+
+    $('#largeCard').append(getCardBodyHTML( /* ... card data ... */ ));
+
+    // Large card has UVI, small ones do not
+    $('#largeCard').append(`<p>UV Index: <span class="uvi ${uviClass(uvi)}">${uvi}</span></p>`);
+
+    // The rest of cards
+    for (let i = 7; i < 40; i += 8) { // The api makes us step by 8 to get the different days in their forecast data
+        $('#fiveDay').append(getCardHTML(/* ... card data ... */));
+    }
+
+    showData();
+
+}
+```
+Here is some information on how I fetched data from the API. ```showData``` & ```hideData``` are used to show and hide cards when data is invalid or when page is starting up.
+```JavaScript
+fetch(queryURLForecast).then(response => response.json())
+    .then(data => {
+        if (data.cod != 404) { // Check location is valid
+
+            buildForecast(/* Data from fetch */);
+
+            // Display location on page
+            $("#location").text(toTitleCase(cityName));
+
+            // Add city data to local storage 
+            addCity(cityName);
+
+        } else
+            hideData();
+
+});
+```
+
+```toTitleCase``` makes the text <em>pretty</em>.
+
+## Screenshots
+
+Minneapolis Weather
+
+![Weather](./Assets/screenshots/screen1.jpg)
+
+5 Day Forecast
+
+![FiveDay](./Assets/screenshots/screen2.png)
+
+## Links
+
+[Webpage](https://andrewtrudeau.github.io/06-weather-dashboard/)
+
+[Github](https://github.com/andrewtrudeau/06-weather-dashboard)
