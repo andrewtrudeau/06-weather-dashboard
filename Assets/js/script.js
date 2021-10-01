f6a0564ec9a52a008980dba996aa683f653b0fb0
TRUDE135 <72237941+TRUDE135@users.noreply.github.com>
2021-09-30 23:40:49 -0500

    changes in readme

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,14 +4,14 @@ const apiKey = "77a5f5e9021668e3908aa9d8669df015"
 
 var cityNames = [];
 
-// if (localStorage.getItem("cityNames")) {
+if (localStorage.getItem("cityNames")) {
 
-//     cityNames = localStorage.getItem("cityNames").split(',');
+    cityNames = localStorage.getItem("cityNames").split(',');
 
-//     for (let i = 0; i < cityNames.length; i++)
-//         addCityButton(cityNames[i]);
+    for (let i = 0; i < cityNames.length; i++)
+        addCityButton(cityNames[i]);
 
-// }
+}
 
 //////////////////////
 // Helper Functions //
